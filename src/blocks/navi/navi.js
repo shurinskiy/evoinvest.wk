e4f4c79354648b15421da4679ac316d3e18111dd
_tango <shurinskiy@mail.ru>
2022-09-05 02:19:10 +0600

    adaptive in process

--- src/blocks/navi/navi.js
+++ src/blocks/navi/navi.js
@@ -1,4 +1,18 @@
 (() => {
+	const $toggle = $('.navi__toggle');
+	const $shell = $('.navi');
+	
+	$toggle.on('click', function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		$shell.toggleClass('opened');
+	});
 
+	$(window).on('click', function(e) {
+		if($shell.hasClass('opened') && !e.target.closest('.navi')) {
+			e.preventDefault();
+			$shell.toggleClass('opened');
+		}
+	});
 
 })();
